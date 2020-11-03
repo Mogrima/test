57da6d83969e8c762fe80f5e6a6249d78b9d504a
Mogrima <rebel.rs@yandex.ru>
2020-11-03 18:12:16 +0400

    доработка таблицы

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,39 @@
 <template>
   <v-app>
     <v-main>
+          <!-- <v-row>
+           
+            
+          </v-row> -->
       <Table :list="workersList" />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import {mapState, mapActions} from 'vuex'
-import Table from '@/components/Table'
+import { mapState, mapActions } from "vuex";
+import Table from "@/components/Table";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    Table
+    Table,
   },
 
   data: () => ({
-    //
+    fireDateСheckbox: true,
   }),
   computed: {
-    ...mapState('workers', {
-      workersList: state => state.list
-    })
+    ...mapState("workers", {
+      workersList: (state) => state.list,
+    }),
   },
   methods: {
-    ...mapActions('workers', [
-      'addWorkers'
-    ])
+    ...mapActions("workers", ["addWorkers"]),
   },
   created() {
-    this.addWorkers()
-  }
+    this.addWorkers();
+  },
 };
 </script>
