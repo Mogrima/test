ab6b10380019dcab7e9a477907b9fee96d4658c3
Mogrima <rebel.rs@yandex.ru>
2020-11-17 21:04:43 +0400

    в компоненте отформатирован код

--- src/components/Table-dialog.vue
+++ src/components/Table-dialog.vue
@@ -9,47 +9,58 @@
     <slot></slot>
     <template v-slot:input>
       <v-container class="lighten-5">
-    <v-row no-gutters class="table__row mb-5">
-      <v-col
-        v-for="value in values"
-        :key="value.label"
-      >
-        <v-text-field
-        
-        v-model="value.v"
-          :label="value.label"
-          hide-details="auto"
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row class="dialog__buttons">
-        <v-col>
-          <v-btn small :text=true class="green--text" @click="cancel">Отменить</v-btn>
+        <v-row no-gutters class="table__row mb-5">
+          <v-col v-for="value in values" :key="value.label">
+            <v-text-field
+              v-model="value.v"
+              :label="value.label"
+              hide-details="auto"
+            ></v-text-field>
           </v-col>
-        <v-col>
+        </v-row>
+        <v-row class="dialog__buttons">
+          <v-col>
+            <v-btn small :text="true" class="green--text" @click="cancel"
+              >Отменить</v-btn
+            >
+          </v-col>
+          <v-col>
           <v-btn small :text=true class="green--text" @click="save">Сохранить</v-btn>
         </v-col>
       </v-row>
   </v-container>
-        
-
     </template>
   </v-edit-dialog>
 </template>
 
 <script>
+// import axios from "axios";
+
 export default {
   name: "TableDialog",
-  props: ["values"],
+  props: {
+    list: {
+      default: () => [],
+      type: Array,
+    },
+    values: {
+      default: () => [],
+      type: Array,
+    },
+  },
+  data() {
+    return {};
+  },
   methods: {
     save() {
-      this.$emit('save', this.values)
-      this.cancel()
+      this.$emit("save", this.values);
+      this.cancel();
+      console.log(this.list[0].id);
     },
     cancel() {
-      this.$refs.dialog.cancel()
+      this.$refs.dialog.cancel();
     },
-  }
+  },
 };
 </script>
 
